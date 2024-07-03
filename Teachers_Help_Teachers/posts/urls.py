a65c5de3f29390fcd0ac8a761656d0f467d8b2c0
Trevor <tahlers@unomaha.edu>
2024-07-02 19:51:37 -0500

    Updated version

--- Teachers_Help_Teachers/posts/urls.py
+++ Teachers_Help_Teachers/posts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("/add", views.add_function, name="add"),
+    path("/search", views.search_function, name="search"),     
 ]
