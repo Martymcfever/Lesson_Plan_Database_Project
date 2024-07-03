6c98ae7a589259cd1212b1d753137a4a0f1d1826
Trevor <tahlers@unomaha.edu>
2024-07-02 20:19:49 -0500

    Updated version

--- Teachers_Help_Teachers/posts/views.py
+++ Teachers_Help_Teachers/posts/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+def add_function(request):
+    return render(request, "add_function.html")
+
+def search_function(request):
+    return render(request, "search_function.html")
