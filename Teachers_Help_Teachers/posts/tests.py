f61ff1d24c0d4cd329434b472e473676ebb64d64
kwcortum <kwcortum@icloud.com>
2024-07-28 22:11:24 -0500

    Updated tests.py with a first test.

--- Teachers_Help_Teachers/posts/tests.py
+++ Teachers_Help_Teachers/posts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+class YourModelTest(TestCase):
+    def setUp(self):
+        YourModel.objects.create(field1='test 1', field2='test 2')
+
+    def test_model_fields_content(self):
+        record = YourModel.objects.get(id=1)
+        expected_field1 = record.field1
+        expected_field2 = record.field2
+        self.assertEqual(expected_field1, 'test 1')
+        self.assertEqual(expected_field2, 'test 2')
