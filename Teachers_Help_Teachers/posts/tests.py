c940b1af93d6b28eeceb5106bc915824ab903d03
kwcortum <kwcortum@icloud.com>
2024-07-29 12:18:42 -0500

    Updated tests.py with a manual test for Post model.

--- Teachers_Help_Teachers/posts/tests.py
+++ Teachers_Help_Teachers/posts/tests.py
@@ -1,13 +1,36 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from .models import Post
+import datetime
 # Create your tests here.
-class YourModelTest(TestCase):
+
+
+class PostModelTestCase(TestCase):
     def setUp(self):
-        YourModel.objects.create(field1='test 1', field2='test 2')
+        dummy_file = SimpleUploadedFile('test.txt', b'Test content')
+
+        self.post = Post.objects.create(title='Some title', pub_date=datetime.datetime.now(),
+                                        contributors='Contributor(s) Name(s)', grade_level='GL', subject='Subject Name',
+                                        description='This is a description', lesson_plan=dummy_file, verified=False)
 
     def test_model_fields_content(self):
-        record = YourModel.objects.get(id=1)
-        expected_field1 = record.field1
-        expected_field2 = record.field2
-        self.assertEqual(expected_field1, 'test 1')
-        self.assertEqual(expected_field2, 'test 2')
+        dummy_file = SimpleUploadedFile('test.txt', b'Test content')
+        record = Post.objects.get(id=self.post.id)
+        expected_field1 = record.title
+        expected_field2 = datetime.datetime.now()
+        expected_field3 = record.contributors
+        expected_field4 = record.grade_level
+        expected_field5 = record.subject
+        expected_field6 = record.description
+        expected_field8 = record.verified
+        self.assertEqual(expected_field1, 'Some title')
+        self.assertEqual(expected_field2, datetime.datetime.now())
+        self.assertEqual(expected_field3, 'Contributor(s) Name(s)')
+        self.assertEqual(expected_field4, 'GL')
+        self.assertEqual(expected_field5, 'Subject Name')
+        self.assertEqual(expected_field6, 'This is a description')
+        self.assertEqual(self.post.lesson_plan.read(), dummy_file.read())
+        self.assertEqual(expected_field8, False)
+
+    def tearDown(self):
+        self.post.delete()
